9905586536d4b32cc6b1d202bac524dd253072a0
jasenpan <jasenpan@gmail.com>
2023-02-19 22:23:52 +1100

    batching

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Client } from "@opensearch-project/opensearch";
+import type { Message } from "node-rdkafka";
 import { KafkaConsumer } from "node-rdkafka";
 
 const OPEN_SEARCH_URL = "http://localhost:9200";
 const OPEN_SEARCH_INDEX = "wikimedia-node";
-const KAFKA_TOPIC_NAME = "wikimedia.recentchange";
+const KAFKA_TOPIC_NAME = "wikimedia.recentchange.nodejs";
+const NUM_MSG_PER_BATCH = 5;
 
 interface IMessage {
   $schema: string;
@@ -63,50 +65,34 @@ async function createOpenSearchIndex(client: Client) {
   }
 }
 
-async function processMessage(openSearchClient: Client, message?: IMessage) {
-  console.log("Message found!  Contents below.", message?.meta);
-  if (!message) {
+async function processMessageInBatch(openSearchClient: Client, messages?: Message[]) {
+  if (!messages || messages.length === 0) {
     return;
   }
 
-  const record = await openSearchClient.index({
-    id: message.meta.id,
-    body: message,
-    index: OPEN_SEARCH_INDEX,
-    refresh: true
+  const body: any[] = [];
+
+  return new Promise((resolve, reject) => {
+    messages.forEach((message) => {
+      const data = JSON.parse(message.value!.toString()) as IMessage;
+      body.push({ index: { _index: OPEN_SEARCH_INDEX, _id: data.meta.id } });
+      body.push(data);
+    });
+
+    openSearchClient.bulk(
+      {
+        body
+      },
+      (err, result) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(result);
+      }
+    );
   });
-
-  console.log(record);
 }
-
-const aa = {
-  $schema: "/mediawiki/recentchange/1.0.0",
-  meta: {
-    uri: "https://commons.wikimedia.org/wiki/Category:January_2023_in_Bosnia_and_Herzegovina",
-    request_id: "88ea7288-0732-4c0d-a2e2-8e528c3dc3cd",
-    id: "c8140da6-f4e2-4cc6-a810-246f8b33831d",
-    dt: "2023-02-16T14:50:36Z",
-    domain: "commons.wikimedia.org",
-    stream: "mediawiki.recentchange",
-    topic: "eqiad.mediawiki.recentchange",
-    partition: 0,
-    offset: 4539442699
-  },
-  id: 2119392811,
-  type: "categorize",
-  namespace: 14,
-  title: "Category:January 2023 in Bosnia and Herzegovina",
-  comment: "[[:File:Зеница 20230104 123632.jpg]] removed from category",
-  timestamp: 1676559036,
-  user: "Ioacc1234red",
-  bot: false,
-  server_url: "https://commons.wikimedia.org",
-  server_name: "commons.wikimedia.org",
-  server_script_path: "/w",
-  wiki: "commonswiki",
-  parsedcomment:
-    '<a href="/wiki/File:%D0%97%D0%B5%D0%BD%D0%B8%D1%86%D0%B0_20230104_123632.jpg" title="File:Зеница 20230104 123632.jpg">File:Зеница 20230104 123632.jpg</a> removed from category'
-};
 
 async function createKafkaConsumer(openSearchClient: Client) {
   const consumer = new KafkaConsumer(
@@ -115,40 +101,29 @@ async function createKafkaConsumer(openSearchClient: Client) {
       "bootstrap.servers": "127.0.0.1:9092"
     },
     {
-      "auto.offset.reset": "earliest"
-      // "enable.auto.commit": false
+      "auto.offset.reset": "earliest",
+      "enable.auto.commit": false
     }
   );
 
   consumer.connect();
 
-  consumer
-    .on("ready", () => {
-      consumer.subscribe([KAFKA_TOPIC_NAME]);
-      console.log("good to go");
-      setInterval(function () {
-        consumer.consume(1);
-      }, 1000);
-      // consumer.consume();
-    })
-    .on("data", (data) => {
-      if (data?.value) {
-        const message = JSON.parse(data?.value?.toString()) as IMessage;
-        processMessage(openSearchClient, message);
-      }
-    })
-    .on("event.error", (e) => {
-      console.log("event.error", e);
-    })
-    .on("connection.failure", (e) => {
-      console.log("connection.failure", e);
-    })
-    .on("disconnected", (e) => {
-      console.log("disconnected", e);
-    })
-    .on("offset.commit", (e) => {
-      console.log("offset.commit", e);
-    });
+  consumer.on("ready", () => {
+    consumer.subscribe([KAFKA_TOPIC_NAME]);
+    console.log("good to go");
+
+    setInterval(async function () {
+      consumer.consume(NUM_MSG_PER_BATCH, async (err, messages) => {
+        if (messages.length === 0) {
+          return;
+        }
+
+        const result = await processMessageInBatch(openSearchClient, messages);
+        console.log("result:: ", messages, result);
+        consumer.commitMessage(messages[messages.length - 1]);
+      });
+    }, 10000);
+  });
 }
 
 async function main() {
